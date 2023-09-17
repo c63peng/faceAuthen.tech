7909f73e74268b4650fc075661819baa1f08fc39
nameerahr <rnameerah@uwaterloo.ca>
2023-09-17 02:20:51 -0400

    Minor changes

--- src/pagerendering.py
+++ src/pagerendering.py
@@ -3,32 +3,32 @@ import pandas as pd
 from taipy.gui import Gui, notify
 import json 
 
+
 webcam_md = """<|toggle|theme|>
+
 
 <container|container|part|
 
 # Welcome, look at the camera to **Sign In**{: .color-primary}
 
-
 <br/>
 
 <card|card p-half|part|
+
 ## **Webcam**{: .color-primary} component
 
+<|text-center|part|
+<|webcam.Webcam|faces={labeled_faces}|classname=face_detector|id=my_face_detector|on_data_receive=handle_image|sampling_rate=100|>
 
-<|Capture|button|on_action={lambda s: s.assign("capture_image", True)}|>
-<|RE-train|button|on_action=button_retrain_clicked|>
 >
 |card>
 |container>
 
-
-<|{show_capture_dialog}|dialog|labels=Validate;Cancel|on_action=on_action_captured_image|title=Add new training image|
-<|{captured_image}|image|width=300px|height=300px|>
-
-<|{captured_label}|input|>
 |>
 """
+
+gui = Gui(webcam_md)
+
 
 with open("src/Attendance.json", 'r') as file:
     attendees = json.load(file)

b2186c62520514a7fcf8e317c7a9c38293895e47
Cindy Peng <cindypeng80@gmail.com>
2023-09-16 21:22:19 -0400

    taipy elements updated

--- src/pagerendering.py
+++ src/pagerendering.py
@@ -8,6 +8,32 @@ import numpy as np
 import pandas as pd
 from taipy.gui import Gui, notify
 
+webcam_md = """<|toggle|theme|>
+
+<container|container|part|
+
+# Welcome, look at the camera to **Sign In**{: .color-primary}
+
+
+<br/>
+
+<card|card p-half|part|
+## **Webcam**{: .color-primary} component
+
+
+<|Capture|button|on_action={lambda s: s.assign("capture_image", True)}|>
+<|RE-train|button|on_action=button_retrain_clicked|>
+>
+|card>
+|container>
+
+
+<|{show_capture_dialog}|dialog|labels=Validate;Cancel|on_action=on_action_captured_image|title=Add new training image|
+<|{captured_image}|image|width=300px|height=300px|>
+
+<|{captured_label}|input|>
+|>
+"""
 
 
 MODEL = "sbcBI/sentiment_analysis_model"
@@ -75,40 +101,6 @@ page_file = """
 <|{dataframe2}|table|width=100%|number_format=%.2f|>
 |>
 
-<br/>
-
-<|{dataframe2}|chart|type=bar|x=Text|y[1]=Score Pos|y[2]=Score Neu|y[3]=Score Neg|y[4]=Overall|color[1]=green|color[2]=grey|color[3]=red|type[4]=line|height=600px|>
-
-"""
-
-
-webcam_md = """<|toggle|theme|>
-
-<container|container|part|
-
-# Welcome, look at the camera to **Sign In**{: .color-primary}
-
-
-<br/>
-
-<card|card p-half|part|
-## **Webcam**{: .color-primary} component
-
-<|text-center|part|
-<|webcam.Webcam|faces={labeled_faces}|classname=face_detector|id=my_face_detector|on_data_receive=handle_image|sampling_rate=100|>
-
-<|Capture|button|on_action={lambda s: s.assign("capture_image", True)}|>
-<|RE-train|button|on_action=button_retrain_clicked|>
->
-|card>
-|container>
-
-
-<|{show_capture_dialog}|dialog|labels=Validate;Cancel|on_action=on_action_captured_image|title=Add new training image|
-<|{captured_image}|image|width=300px|height=300px|>
-
-<|{captured_label}|input|>
-|>
 """
 
 
@@ -124,7 +116,6 @@ def analyze_file(state) -> None:
     with open(state.path, "r", encoding="utf-8") as f:
         data = f.read()
         print(data)
-
         file_list = list(data.split("\n"))
 
     for i, input_text in enumerate(file_list):
